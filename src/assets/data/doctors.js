16cad0dbc7ffd71e8fc8c5b3f2c27ef7379d746d
6SHREYA <115722158+6SHREYA@users.noreply.github.com>
2023-12-25 14:32:40 +0530

    Add files via upload

---
+++ src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. X",
+    specialization: "Surgeon",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "YASHODA HOSPITAL, GHAZIABAD.",
+  },
+  {
+    id: "02",
+    name: "Dr. Y",
+    specialization: "Neurologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg02,
+    totalPatients: 1500,
+    hospital: "YASHODA HOSPITAL, GHAZIABAD.",
+  },
+  {
+    id: "03",
+    name: "Dr. Z",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "YASHODA HOSPITAL, GHAZIABAD.",
+  },
+];
